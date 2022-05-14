72e85f8558823dc4a547b47b9dac2a7267dc7689
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2022-05-14 10:22:15 -0300

    Subindo metodo select para as classes Agencia e ContaCorrente

---
+++ Infraestrutura.Testes/AgenciaRepositorioTestes.cs
@@ -0,0 +1,51 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Infraestrutura.Testes
+{
+    public class AgenciaRepositorioTestes
+    {
+        private readonly IAgenciaRepositorio _agenciaRepositorio;
+
+        public AgenciaRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IAgenciaRepositorio, AgenciaRepositorio>();
+
+            var provedor = servico.BuildServiceProvider();
+            _agenciaRepositorio = provedor.GetService<IAgenciaRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodasAgencias()
+        {
+            List<Agencia> lista = _agenciaRepositorio.ObterTodos();
+
+            Assert.NotNull(lista);
+        }
+
+        [Fact]
+        public void TestaObterAgenciaPorId()
+        {
+            var agencia = _agenciaRepositorio.ObterPorId(9);
+            Assert.NotNull(agencia);
+        }
+
+        [Theory]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void TestaObterAgenciaPorVariosId(int id)
+        {
+            var agencia = _agenciaRepositorio.ObterPorId(id);
+            Assert.NotNull(agencia);
+        }
+    }
+}
---
+++ Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
@@ -0,0 +1,53 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Infraestrutura.Testes
+{
+    public class ContaCorrenteRepositorioTestes
+    {
+        private readonly IContaCorrenteRepositorio _contaCorrenteRepositorio;
+
+        public ContaCorrenteRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IContaCorrenteRepositorio, ContaCorrenteRepositorio>();
+
+            var provedor = servico.BuildServiceProvider();
+            _contaCorrenteRepositorio = provedor.GetService<IContaCorrenteRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodasAgencias()
+        {
+            List<ContaCorrente> lista = _contaCorrenteRepositorio.ObterTodos();
+
+            Assert.NotNull(lista);
+        }
+
+        [Fact]
+        public void TestaObterAgenciaPorId()
+        {
+            var agencia = _contaCorrenteRepositorio.ObterPorId(4);
+            Assert.NotNull(agencia);
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void TestaObterAgenciaPorVariosId(int id)
+        {
+            var agencia = _contaCorrenteRepositorio.ObterPorId(id);
+            Assert.NotNull(agencia);
+        }
+
+
+    }
+}
